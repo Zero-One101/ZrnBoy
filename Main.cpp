f894e16d41b7dad1f2a733e337715945e40ac63f
Stephen Badrick <stephenbadrick@gmail.com>
2017-04-27 14:14:07 +0100

    Integrated Qt to project

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,11 @@
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFileDialog>
+#include <string.h>
+
 int main(int argc, char* argv[])
 {
+	QApplication app(argc, argv);
+	std::string fileName = QFileDialog::getOpenFileName(NULL, "Open File", NULL, "Gameboy ROMs (*.gb)").toStdString();
+
 	return 0;
 }
